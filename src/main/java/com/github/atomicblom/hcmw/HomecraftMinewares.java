b1375b7d6c24ecaa6f1a80255808f55f6dd6ff51
Steven Blom <atomicblom@binaryvibrance.net>
2016-12-22 01:33:11 +1030

    Add Debug flag

--- src/main/java/com/github/atomicblom/hcmw/HomecraftMinewares.java
+++ src/main/java/com/github/atomicblom/hcmw/HomecraftMinewares.java
@@ -15,11 +15,11 @@ public class HomecraftMinewares
 {
     public static final String MODID = "hcmw";
     public static final String VERSION = "1.0";
+    public static final boolean DEBUG = false;
 
     @EventHandler
     public void preInit(FMLPreInitializationEvent event) {
         StructureRegistry.setMOD_ID(MODID);
-
     }
 
     @EventHandler
@@ -27,13 +27,18 @@ public class HomecraftMinewares
     {
         StructureRegistry.loadRegisteredPatterns();
         MinecraftForge.EVENT_BUS.register(new HighlightPreview());
-        MinecraftForge.EVENT_BUS.register(new HighlightBoundingBoxDebug());
+        if (DEBUG)
+        {
+            MinecraftForge.EVENT_BUS.register(new HighlightBoundingBoxDebug());
+        }
     }
 
     @EventHandler
     public static void serverStart(FMLServerStartingEvent event)
     {
-        //TODO: Only load this if debug mode is enabled.
-        event.registerServerCommand(new StructureRegistry.CommandReloadStructures());
+        if (DEBUG)
+        {
+            event.registerServerCommand(new StructureRegistry.CommandReloadStructures());
+        }
     }
 }
