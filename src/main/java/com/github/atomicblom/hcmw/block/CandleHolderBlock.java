e63b75bee3f61f9bf401825ff5e21d60b1ff7acd
Steven Blom <atomicblom@binaryvibrance.net>
2016-12-23 22:14:23 +1030

    Fix candle holder collisions

--- src/main/java/com/github/atomicblom/hcmw/block/CandleHolderBlock.java
+++ src/main/java/com/github/atomicblom/hcmw/block/CandleHolderBlock.java
@@ -4,12 +4,17 @@ import net.minecraft.block.BlockHorizontal;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.Entity;
 import net.minecraft.util.EnumParticleTypes;
+import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
+
+import javax.annotation.Nullable;
+import java.util.List;
 import java.util.Random;
 
 public class CandleHolderBlock extends BlockHorizontal
@@ -29,6 +34,18 @@ public class CandleHolderBlock extends BlockHorizontal
     }
 
     @Override
+    @Deprecated
+    public AxisAlignedBB getBoundingBox(IBlockState state, IBlockAccess source, BlockPos pos) {
+        float pixel = 1/16f;
+        return new AxisAlignedBB(4 * pixel, 0, 4 * pixel, 1-4*pixel, 8 * pixel,1-4*pixel);
+    }
+
+    @Override
+    @Deprecated
+    public void addCollisionBoxToList(IBlockState state, World worldIn, BlockPos pos, AxisAlignedBB entityBox, List<AxisAlignedBB> collidingBoxes, @Nullable Entity entityIn) {
+    }
+
+    @Override
     public int getLightValue(IBlockState state, IBlockAccess world, BlockPos pos) {
         return 7;
     }
@@ -38,7 +55,7 @@ public class CandleHolderBlock extends BlockHorizontal
     public void randomDisplayTick(IBlockState stateIn, World worldIn, BlockPos pos, Random rand)
     {
         final double x = pos.getX() + 0.5D;
-        final double y = pos.getY() + 0.5D + 0.32D;
+        final double y = pos.getY() + 0.5D + 0.0D;
         final double z = pos.getZ() + 0.5D;
 
         worldIn.spawnParticle(EnumParticleTypes.SMOKE_NORMAL, x, y, z, 0.0D, 0.0D, 0.0D);
