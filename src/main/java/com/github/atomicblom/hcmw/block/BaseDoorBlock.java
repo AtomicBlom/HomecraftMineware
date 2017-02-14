f01a85ce7d5d937c34f9ea5779c259c7bcdf0ab5
Steven Blom <atomicblom@binaryvibrance.net>
2017-02-15 00:45:31 +1030

    Fix particles and Collision Box

--- src/main/java/com/github/atomicblom/hcmw/block/BaseDoorBlock.java
+++ src/main/java/com/github/atomicblom/hcmw/block/BaseDoorBlock.java
@@ -4,10 +4,12 @@ import com.foudroyantfactotum.tool.structure.block.StructureBlock;
 import com.foudroyantfactotum.tool.structure.tileentity.StructureTE;
 import com.foudroyantfactotum.tool.structure.utility.StructureDefinitionBuilder;
 import com.google.common.collect.ImmutableMap;
+import net.minecraft.block.Block;
 import net.minecraft.block.state.BlockStateContainer;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.EnumFacing;
+import net.minecraft.util.EnumParticleTypes;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import javax.annotation.Nullable;
@@ -36,7 +38,31 @@ public class BaseDoorBlock extends StructureBlock
     @Override
     public void spawnBreakParticle(World world, StructureTE te, BlockPos local, float sx, float sy, float sz)
     {
+        final IBlockState state = world.getBlockState(te.getPos());
 
+        for (int x = 0; x < 4; ++x)
+        {
+            for (int y = 0; y < 4; ++y)
+            {
+                for (int z = 0; z < 4; ++z)
+                {
+                    final double particleX = local.getX() + (x + 0.5D) / 4.0D;
+                    final double particleY = local.getY() + (y + 0.5D) / 4.0D;
+                    final double particleZ = local.getZ() + (z + 0.5D) / 4.0D;
+
+                    world.spawnParticle(
+                            EnumParticleTypes.BLOCK_CRACK,
+                            particleX,
+                            particleY,
+                            particleZ,
+                            particleX - local.getX() - 0.5D,
+                            particleY - local.getY() - 0.5D,
+                            particleZ - local.getZ() - 0.5D,
+                            Block.getStateId(state)
+                    );
+                }
+            }
+        }
     }
 
     @Nullable
@@ -79,13 +105,7 @@ public class BaseDoorBlock extends StructureBlock
         final float pixelWidth = 0.0625f;
 
         builder.setCollisionBoxes(
-                //Head board
-                new float[] {0.0f, 0.0f, 0.0f, 2f, 1.4f, pixelWidth * 2},
-                //Posts
-                new float[] {2.0f, 0.0f, 2.0f, 2-pixelWidth*3, 1.0f-pixelWidth*1.5f, 2-pixelWidth*3},
-                new float[] {0.0f, 0.0f, 2.0f, pixelWidth*3, 1.0f-pixelWidth*1.5f, 2-pixelWidth*3},
-                //Matress
-                new float[] {pixelWidth * 3, pixelWidth * 6, 0.0f, 2.0f-pixelWidth * 3, pixelWidth * 11, 2.0f-pixelWidth * 2}
+                new float[] {0.0f, 0.0f, 1-pixelWidth * 3, 2.0f, 3.0f, 1}
         );
 
         return builder;
