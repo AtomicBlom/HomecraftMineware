59dc83a056b1c569ae1b00fcb411978352b5abdf
Steven Blom <atomicblom@binaryvibrance.net>
2017-02-17 21:58:15 +1030

    Fix rendering of doors

--- src/main/java/com/github/atomicblom/hcmw/block/BaseDoorBlock.java
+++ src/main/java/com/github/atomicblom/hcmw/block/BaseDoorBlock.java
@@ -8,6 +8,7 @@ import net.minecraft.block.Block;
 import net.minecraft.block.state.BlockStateContainer;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.EnumBlockRenderType;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumParticleTypes;
 import net.minecraft.util.math.BlockPos;
@@ -63,6 +64,13 @@ public class BaseDoorBlock extends StructureBlock
                 }
             }
         }
+    }
+
+    @Override
+    @Deprecated
+    public EnumBlockRenderType getRenderType(IBlockState state)
+    {
+        return EnumBlockRenderType.MODEL;
     }
 
     @Nullable
